be8fe1ce3c2a09922bbed2f4530079c3915434c1
Nick <delta2888@gmail.com>
2023-11-23 15:21:05 +1100

    build: :heavy_plus_sign: add react-hot-toast

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -4,13 +4,17 @@ import { NextUIProvider } from '@nextui-org/react';
 
 import { SessionProvider } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import { Toaster } from 'react-hot-toast';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
 
   return (
     <SessionProvider>
-      <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+      <NextUIProvider navigate={router.push}>
+        {children}
+        <Toaster />
+      </NextUIProvider>
     </SessionProvider>
   );
 };
