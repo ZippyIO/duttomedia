bdb0493be68d1fc329d2f6a9bd1c747ed5738bbd
Nick <delta2888@gmail.com>
2023-11-23 12:55:04 +1100

    feat: :sparkles: add signIn callback to next-auth to disable new user sign up
    Disable user sign up if they don't already exist in db. If db is wiped this will need to be disabled so the user can be re added. Should look into a setting in UI

--- src/lib/nextauth.ts
+++ src/lib/nextauth.ts
@@ -4,6 +4,7 @@ import { type PrismaClient } from '@prisma/client';
 import NextAuth, { type NextAuthConfig } from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 import { NextResponse } from 'next/server';
+import z from 'zod';
 
 import db from '~/lib/db';
 
@@ -17,6 +18,25 @@ export const authConfig = {
   ],
   session: { strategy: 'jwt' },
   callbacks: {
+    signIn: async ({ account }) => {
+      //? Check if user exists in database
+      //? if not return false to prevent sign in
+      const pId = account?.providerAccountId;
+      const id = z.string().min(1).safeParse(pId);
+      if (!id.success) {
+        return false;
+      }
+
+      const isExistingUser = await db.account.findFirst({
+        where: { providerAccountId: id.data },
+      });
+
+      if (!isExistingUser) {
+        return false;
+      }
+
+      return true;
+    },
     authorized: ({ request, auth }) => {
       if (!auth?.user) {
         const url = request.nextUrl.toString();
